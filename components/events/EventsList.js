9c61d5f7043cb0a73a029df26c12e0461d3556c8
Karol Galera <frontend.galera@gmail.com>
2023-01-29 17:10:48 +0100

    title props added to EventList

--- components/events/EventsList.js
+++ components/events/EventsList.js
@@ -8,6 +8,7 @@ const EventsList = ({ items }) => {
           key={event.id}
           id={event.id}
           image={event.image}
+          title={event.title}
           date={event.date}
           location={event.location}
         />
