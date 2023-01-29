975e7fa675db7dd3434e9d982af7f3623b7eb169
Karol Galera <frontend.galera@gmail.com>
2023-01-29 17:07:36 +0100

    EventList component added to index.js and getFeateruedEvents function imported to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import EventsList from "../components/events/EventsList";
+import { getFeaturedEvents } from "../dummy-data";
+
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventsList items={featuredEvents} />
     </div>
   );
 };
