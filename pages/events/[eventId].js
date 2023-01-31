2694c49c58bee26de14e3f9c5129a13654ba7b2a
Karol Galera <frontend.galera@gmail.com>
2023-01-31 19:42:18 +0100

    useRouter hook and finding events by Id added to EventDeatailPage route component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,29 @@
+import { useRouter } from "next/router";
+import EventContent from "../../components/eventDetail/EventContent/eventContent";
+import EventLogistics from "../../components/eventDetail/EventLogistics/EventLogistics";
+import EventSummary from "../../components/eventDetail/EventSummary/EventSummary";
+import { getEventById } from "../../dummy-data";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <div>Event not found !!!</div>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </>
   );
 };
 
