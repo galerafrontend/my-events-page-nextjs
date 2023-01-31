0549d898169b59ea1e05b84683279f5e2179579e
Karol Galera <frontend.galera@gmail.com>
2023-01-31 19:06:34 +0100

    styles and icons added to EventItem component

---
+++ components/events/EventItem/EventItem.js
@@ -0,0 +1,41 @@
+import AddressIcon from "../../icons/addressIcon";
+import ArrowRightIcon from "../../icons/arrowRightIcon";
+import DateIcon from "../../icons/dateIcon";
+import Button from "../../ui/Button";
+import classes from "./styles.module.css";
+
+const EventItem = ({ id, title, image, date, location }) => {
+  const formmatedDate = new Date(date).toLocaleDateString("en-Us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div className={classes.date}>
+          <DateIcon />
+          <time>{formmatedDate}</time>
+        </div>
+        <div className={classes.address}>
+          <AddressIcon />
+          <address>{location}</address>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <Button link={exploreLink}>
+          <span>Explore Link <ArrowRightIcon /></span>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
