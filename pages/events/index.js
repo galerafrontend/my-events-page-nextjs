9a0cd822eef2d778ceab6d6fda4a2e2d75912a73
Karol Galera <frontend.galera@gmail.com>
2023-02-04 18:51:52 +0100

    function getAllEvents and component EventList added to AllEventsPage route component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import EventsList from "../../components/events/EventsList/EventsList";
+import { getAllEvents } from "../../dummy-data";
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>AllEvents</h1>
+      <EventsList items={events} />
     </div>
   );
 };
