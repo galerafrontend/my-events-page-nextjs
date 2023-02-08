d0074e3b5450ee185b7c27025e32531ec9695b8a
Karol Galera <frontend.galera@gmail.com>
2023-02-08 19:52:14 +0100

    conditon that checks is filterData valid and condition that checks is filteredEvents function complited added to FilteredEventsPage route component

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,41 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numberYear = +filteredYear;
+  const numberMonth = +filteredMonth;
+
+  if (
+    isNaN(numberYear) ||
+    isNaN(numberMonth) ||
+    numberYear > 2030 ||
+    numberYear < 2021 ||
+    numberMonth > 12 ||
+    numberMonth < 1
+  ) {
+    return <p>Invalid filter. Please adjust your values.</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numberYear,
+    month: numberMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
