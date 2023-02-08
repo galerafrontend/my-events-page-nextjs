f3674af9d70a342526a0fdabd3cc8fbcb4da3ce7
Karol Galera <frontend.galera@gmail.com>
2023-02-08 19:36:54 +0100

    function that finds events by year and month added to AllEventsPage

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
+import { useRouter } from "next/router";
 import EventsList from "../../components/events/EventsList/EventsList";
 import EventsSearch from "../../components/events/EventsSearch/EventsSearch";
 import { getAllEvents } from "../../dummy-data";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+  const findEventHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />    
+      <EventsSearch onSearch={findEventHandler} />
       <EventsList items={events} />
     </>
   );
