9bd6f7e45811c33a1f8d0b9da81cf409eac0182b
Karol Galera <frontend.galera@gmail.com>
2023-02-28 16:18:01 +0100

    Head next component added to dynamic FilteredEventsPage route component

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,11 +3,23 @@ import { getFilteredEvents } from "../../helpers/apiUtil";
 import ResultsTitle from "../../components/events/ResultsTitle/ResultsTitle";
 import ErrorAlert from "../../components/ui/ErrorAlert/ErrorAlert";
 import Button from "../../components/ui/Button/Button";
+import Head from "next/head";
 
 const FilteredEventsPage = ({ hasError, events, numberDate }) => {
+  const pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta
+        name="description"
+        content={`All events for ${numberDate.month}/${numberDate.year}.`}
+      />
+    </Head>
+  );
+
   if (hasError) {
     return (
       <>
+        {pageHeadData}
         <div className="center">
           <ErrorAlert>Invalid filter. Please adjust your values.</ErrorAlert>
           <Button link="/events">Back to all events</Button>
@@ -21,6 +33,7 @@ const FilteredEventsPage = ({ hasError, events, numberDate }) => {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <>
+        {pageHeadData}
         <div className="center">
           <ErrorAlert>No events found for the chosen filter!</ErrorAlert>
           <Button link="/events">Back to all events</Button>
@@ -33,6 +46,7 @@ const FilteredEventsPage = ({ hasError, events, numberDate }) => {
 
   return (
     <>
+      {pageHeadData}
       <ResultsTitle date={date} />
       <EventsList items={filteredEvents} />
     </>
