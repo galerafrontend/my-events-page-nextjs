76b8532d04541dc6695c7394cb531be0cbc35e7f
Karol Galera <frontend.galera@gmail.com>
2023-02-28 16:00:01 +0100

    Head next component added to pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import EventsList from "../components/events/EventsList/EventsList";
 import { getFeaturedEvents } from "../helpers/apiUtil";
+import Head from "next/head";
 
 const HomePage = ({ events }) => {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventsList items={events} />
     </div>
   );
