e0d5969a8beca0a864dd970e8586a3b3d195c943
Karol Galera <frontend.galera@gmail.com>
2023-03-03 18:39:36 +0100

    Comments component with styles added

---
+++ components/input/Comments/Comments.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import CommentList from "../CommentList/CommentsList";
+import NewComment from "../NewComment/NewComment";
+import classes from "./styles.module.css";
+
+const Comments = ({ eventId }) => {
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+};
+
+export default Comments;
