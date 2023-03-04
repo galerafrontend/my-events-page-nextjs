0872ecc11d1c5fa7ebc1b699a52b807ede5ca079
Karol Galera <frontend.galera@gmail.com>
2023-03-04 13:51:37 +0100

    dynamic api route that storing and fetching comments, added

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,52 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://frontendgalera:Q4esdRa8r-7zAZr@cluster0.vibpqwp.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
+
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
+
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
+    newComment.id = result.insertedId;
+    res.status(201).json({ message: "Comment added", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    client.close();
+
+    res.status(200).json({ comments: documents });
+  }
+};
+
+export default handler;
