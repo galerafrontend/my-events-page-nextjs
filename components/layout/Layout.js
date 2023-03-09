d99786d7502208cb950a40fcff73bff6315b8b1e
Karol Galera <frontend.galera@gmail.com>
2023-03-09 11:32:37 +0100

    useContext hook and condition that shows Notification, added to Layout component

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+import NotificationContext from "../../store/notificationContext";
 import MainHeader from "./MainHeader/MainHeader";
+import Notification from "../ui/Notification/Notification";
 
 const Layout = ({ children }) => {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 };
